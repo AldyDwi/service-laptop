63bd3bbda13da60d0e98b1bb8de5097916d19194
aldydwi <ardiansyahaldy4@gmail.com>
2024-11-27 23:58:37 +0700

    Tambah icon website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Fath Comp</title>
+        <link rel="icon" type="image/svg+xml" href="{{ asset('gambar/forge-icon.svg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
